4eda9745808829e807652af584c9827b52754d2e
otto <otto.o@cybertekschool.com>
2021-11-10 13:34:17 +0300

    user create commit

--- src/main/java/com/cybertek/controller/UserController.java
+++ src/main/java/com/cybertek/controller/UserController.java
@@ -3,6 +3,8 @@ package com.cybertek.controller;
 import com.cybertek.dto.RoleDTO;
 import com.cybertek.dto.UserDTO;
 import com.cybertek.implementation.RoleServiceImpl;
+import com.cybertek.service.RoleService;
+import com.cybertek.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,14 +15,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/user")
 public class UserController {
 
-    //@Autowired
+    @Autowired
+    RoleService roleService;
+    @Autowired
+    UserService userService;
 
     @GetMapping("/create")
     public String createUser(Model model){
 
         model.addAttribute("user", new UserDTO());
 
-        model.addAttribute("roleList", new RoleServiceImpl().findAll());
+        model.addAttribute("roles", roleService.findAll());
+
+        model.addAttribute("users", userService.findAll());
 
         return "/user/create";
     }
