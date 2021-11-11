55de1fa2eea44b58d9a6082435388cf2170d68e1
otto <otto.o@cybertekschool.com>
2021-11-11 10:31:13 +0300

    project save

--- src/main/java/com/cybertek/controller/ProjectController.java
+++ src/main/java/com/cybertek/controller/ProjectController.java
@@ -1,12 +1,14 @@
 package com.cybertek.controller;
 
 import com.cybertek.dto.ProjectDTO;
+import com.cybertek.enums.Status;
 import com.cybertek.service.ProjectService;
 import com.cybertek.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -28,6 +30,17 @@ public class ProjectController {
         model.addAttribute("managers", userService.findAll());
 
         return "/project/create";
+
+    }
+
+    @PostMapping("/create")
+    public String insertProject(Model model, ProjectDTO project){
+
+        projectService.save(project);
+
+        project.setProjectStatus(Status.OPEN);
+
+        return "redirect:/project/create";
 
     }
 
